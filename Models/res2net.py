e2157e2f59f837e9f3cbb7af2aa6e98dca39121d
Vedanth <vedanth.1113@gmail.com>
2022-04-24 12:29:40 +0530

    print res2net-3

--- Models/res2net.py
+++ Models/res2net.py
@@ -222,7 +222,7 @@ def res2net101_v1b(pretrained=True, **kwargs):
     if True:
         print("loading res2net cls pretrained model")
         print(os.getcwd())
-        pretrained = torch.load(open("./res2net101_v1b_26w_4s-0812c246.pth", "rb"))
+        pretrained = torch.load(open("./Models/res2net101_v1b_26w_4s-0812c246.pth", "rb"))
         print("loaded res2net pretrained model")
     return model
 
